e7a59072c8b596bab576f359a75339ccf8ca0e31
Frank Nguyen <xsensevwx@gmail.com>
2022-04-14 14:13:38 +0700

    Update script

--- wsl_install.sh
+++ wsl_install.sh
@@ -1,5 +1,4 @@
 #docker install
-sudo groupadd docker
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
